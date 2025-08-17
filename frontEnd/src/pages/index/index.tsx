5d438d5b00d642a360e80aad1717853348f65c95
watchhill101 <1443256811@qq.com>
2025-08-17 22:05:43 +0800

    修

--- frontEnd/src/pages/index/index.tsx
+++ frontEnd/src/pages/index/index.tsx
@@ -1,21 +1,21 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
-import { SafeButton } from '../../utils/platform'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+import { SafeButton } from "../../utils/platform";
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
-    console.log('智能充电应用启动')
-  })
+    console.log("智能充电应用启动");
+  });
 
   return (
-    <View className='index'>
-      <Text className='welcome-text'>智能充电</Text>
-      
+    <View className="index">
+      <Text className="welcome-text">智能充电</Text>
+
       {/* 使用安全的按钮组件，自动适配不同平台 */}
-      <SafeButton type='primary' className='demo-button'>
+      <SafeButton type="primary" className="demo-button">
         开始使用
       </SafeButton>
     </View>
-  )
+  );
 }
