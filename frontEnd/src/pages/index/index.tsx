ee36ba263c9ad12f69a8f6a544edc86046d5f43a
watchhill101 <1443256811@qq.com>
2025-08-18 10:09:31 +0800

    二维码扫描

--- frontEnd/src/pages/index/index.tsx
+++ frontEnd/src/pages/index/index.tsx
@@ -2,20 +2,23 @@ import { View, Text } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
 import "./index.scss";
 import { SafeButton } from "../../utils/platform";
+import Taro from "@tarojs/taro";
 
 export default function Index() {
   useLoad(() => {
     console.log("智能充电应用启动");
   });
+  const theScan = () => {
+    // 跳转到扫码页面
+    Taro.navigateTo({
+      url: "/pages/scan/index",
+    });
+  };
 
   return (
     <View className="index">
       <Text className="welcome-text">智能充电</Text>
-
-      {/* 使用安全的按钮组件，自动适配不同平台 */}
-      <SafeButton type="primary" className="demo-button">
-        开始使用
-      </SafeButton>
     </View>
   );
 }
+//scan
